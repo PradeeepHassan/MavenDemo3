8d2ab7b45bd0e52af8f73fb7449cd7585e506f1c
PradeeepHassan <pradeepip88@gmail.com>
2018-07-02 21:37:25 +0530

    its my 3rd time code updtae

--- src/main/java/com/qa/pradeep/baseclass/BaseClassACT.java
+++ src/main/java/com/qa/pradeep/baseclass/BaseClassACT.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -13,10 +14,10 @@ public static WebDriver driver;
 	
 	@BeforeMethod	
 	public void precondtionAT(){
-    	System.setProperty("webdriver.chrome.driver","I:\\andrea automation workspace\\Andrea Automation\\exefiles\\chromedriver.exe");
-    	driver = new ChromeDriver();		
-//		System.setProperty("webdriver.gecko.driver","I:\\andrea automation workspace\\Andrea Automation\\exefiles\\geckodriver.exe");
-//		WebDriver driver= new FirefoxDriver();
+//    	System.setProperty("webdriver.chrome.driver","I:\\andrea automation workspace\\Andrea Automation\\exefiles\\chromedriver.exe");
+//    	driver = new ChromeDriver();		
+		System.setProperty("webdriver.gecko.driver","I:\\andrea automation workspace\\Andrea Automation\\exefiles\\geckodriver.exe");
+		WebDriver driver= new FirefoxDriver();
 		driver.get("http://127.0.0.1/login.do");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
